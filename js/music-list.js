aba9c33521e191708530960449a42f27f30f6c2b
johnjc153 <153johnjc@gmail.com>
2021-05-09 04:19:54 -0700

    added songs array

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Montero",
+        artist: "Lil Nas X",
+        img: "music1",
+        src: "music1"
+    },
+    {
+        name: "Buy You a Drank",
+        artist: "T-Pain",
+        img: "music2",
+        src: "music2"
+    },
+    {
+        name: "Montero",
+        artist: "Lil Nas X",
+        img: "music1",
+        src: "music1"
+    },
+    {
+        name: "Buy You a Drank",
+        artist: "T-Pain",
+        img: "music2",
+        src: "music2"
+    },
+    {
+        name: "Montero",
+        artist: "Lil Nas X",
+        img: "music1",
+        src: "music1"
+    },
+    {
+        name: "Buy You a Drank",
+        artist: "T-Pain",
+        img: "music2",
+        src: "music2"
+    },
+]
